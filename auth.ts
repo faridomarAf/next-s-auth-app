e7f822a0044871c66208e658e38f93d744334575
Farid Omar <fariedomar43@gmail.com>
2024-12-03 16:48:44 +0530

    create middelware & configure routes

---
+++ auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+import {PrismaAdapter} from '@auth/prisma-adapter'
+import { db } from "./utils/dbConfig";
+ 
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(db),
+  session:{strategy:"jwt"},
+  ...authConfig,
+})
