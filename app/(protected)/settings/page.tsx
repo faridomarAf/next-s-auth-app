6de196646482389d39ae050b91a6d7d27a595b5e
Farid Omar <fariedomar43@gmail.com>
2024-12-03 17:30:32 +0530

    create the logout functionality at settings page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import {auth} from '@/auth'
+import {auth, signOut} from '@/auth'
+import { Button } from '@/components/ui/button';
 
 export default async function SettingsPage() {
     const session = await auth();
   return (
     <div>
         {JSON.stringify(session)}
+        <form action={async()=>{
+          "use server"
+          await signOut();
+        }}>
+          <Button type='submit'>
+            Sign out
+          </Button>
+        </form>
     </div>
   )
 }
