2f9c335c4a0183a2ee9c6f64b4bf4aa5f1c497fb
Farid Omar <fariedomar43@gmail.com>
2024-12-12 15:24:37 +0530

    implement SignIn functionality throwgh social.tsx file

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -2,14 +2,31 @@
 import {FaGithub} from 'react-icons/fa';
 import {FcGoogle} from 'react-icons/fc';
 import { Button } from '../ui/button';
+import { signIn } from 'next-auth/react';// its imported from ['next-auth/react'] because it use in ['use client'] component
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 
 export default function Social() {
+  const onClick = (provider: 'google' | 'github')=>{
+     signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT
+     })
+  };
+
+
   return (
     <div className='flex w-full gap-2'>
-        <Button className='w-full' variant={'outline'}>
+        <Button 
+          className='w-full' 
+          variant={'outline'}
+          onClick={()=> onClick('google')}
+          >
            <FcGoogle className='w-5 h-5'/>
         </Button>
-        <Button className='w-full' variant={'outline'}>
+        <Button 
+          className='w-full' 
+          variant={'outline'}
+          onClick={()=> onClick('github')}
+        >
            <FaGithub className='w-5 h-5'/>
         </Button>
     </div>
