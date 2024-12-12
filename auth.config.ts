3b1b89eafabde4f4bf3a41f3d0e762acfd0c1d0d
Farid Omar <fariedomar43@gmail.com>
2024-12-12 14:49:20 +0530

    add client-id & client-secret for github and google at auth-config.ts file

--- auth.config.ts
+++ auth.config.ts
@@ -3,10 +3,24 @@ import Credentials from 'next-auth/providers/credentials';
 import { LoginSchema } from "./schemas";
 import { getUserByEmail } from "./utils/user";
 import bcrypt from 'bcryptjs';
+import Github from 'next-auth/providers/github'
+import Google from 'next-auth/providers/google'
 
  
 export default {
      providers: [
+      //login with github
+      Github({
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET
+      }),
+      
+      //login with google
+      Google({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      }),
+
       //login with credentials
       Credentials({
          async authorize(credentials){
