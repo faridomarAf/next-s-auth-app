d5f20a1c1dfe90cfe10c52edfdbad20648c98219
Farid Omar <fariedomar43@gmail.com>
2024-12-21 12:32:40 +0530

    create a twoFactorToken-generator by generatePasswordResetToken at token.ts file

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,7 +1,9 @@
 import { db } from '@/utils/dbConfig';
+import crypto from 'crypto';
 import { getPasswordResetTokenByEmail } from '@/utils/password-token-reset';
 import { getVirficationTokenByEmail } from '@/utils/verification-token';
 import {v4 as uuidv4} from 'uuid';
+import { getTwoFactorTokenByEmail } from '@/utils/two-factor-token';
 
 export const generateVerificationToken = async(email: string)=>{
     // here we have to generate the actual token, for that, we should install [npm i uuid]
@@ -53,4 +55,32 @@ export const generatePasswordResetToken = async(email: string)=>{
         }
     })
     return passwordResetToken;
+}
+
+
+//generate two-factor-token
+export const generateTwoFactorToken = async(email: string)=>{
+    // create a token which is 6 digit number
+    const token = crypto.randomInt(100_000, 1_000_000).toString();
+    const expires = new Date(new Date().getTime()+ 3600 * 1000);
+
+    //check for existingToken
+    const existingToken = await getTwoFactorTokenByEmail(email);
+
+    if(existingToken){
+        await db.twoFactorToken.delete({
+            where:{id: existingToken.id}
+        })
+    }
+
+    //create new twoFactorToken
+    const twoFactorToken = await db.twoFactorToken.create({
+        data:{
+            email,
+            token,
+            expires
+        }
+    });
+
+    return twoFactorToken;
 }
